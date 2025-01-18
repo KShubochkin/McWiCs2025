b277933330f03844462d67cf6f904d4ffc338b9d
KShubochkin <kshubochkin2022@gmail.com>
2025-01-18 15:54:49 -0500

    in progress

--- src/Inventory.java
+++ src/Inventory.java
@@ -3,4 +3,14 @@ import java.util.Map;
 
 public class Inventory {
     private Map<Ingredient,Double> items = new HashMap<>();
+    public void addItem(Ingredient item, double amount){
+        //check if item exists
+        //if so, add
+        //if not, ask if you want to create ingredient (trigger event), plus show "similar results"
+        ingredientNotFound(item);
+    }
+    private void ingredientNotFound(Ingredient item){
+        //search existing ingredients for similarly spelled items, or items that contain the input (if we have time lmao)
+
+    }
 }
