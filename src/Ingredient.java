165ba2e82c350383d802006102637fd1ede20743
joannie-lao <joannie.lao@gmail.com>
2025-01-18 16:31:10 -0500

    Update Ingredient.java

--- src/Ingredient.java
+++ src/Ingredient.java
@@ -4,14 +4,6 @@ public class Ingredient {
     private Unit unit;
     private String name;
     private Set<Tag> tags;
-
-    public void addIngredient(String ingredient, Unit unit, String[] tags){
-        //Create a new ingredient with the corresponding ingredient name, unit type, and appropriate tags
-        this.name = ingredient;
-        this.unit = unit;
-        for (int i = 0; i < tags.length; i++){
-                this.tags.add(tags[i]);
-        }
-    }
+    
         
 }
