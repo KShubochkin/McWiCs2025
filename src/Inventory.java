d53a3d0b2ecd5fc1149ac6d6f11a8780dfd48231
KShubochkin <kshubochkin2022@gmail.com>
2025-01-18 17:10:50 -0500

    working on how to handle an "event," in this case adding an ingredient and asking the user if they want to create a new one

--- src/Inventory.java
+++ src/Inventory.java
@@ -6,11 +6,20 @@ public class Inventory {
     public void addItem(Ingredient item, double amount){
         //check if item exists
         //if so, add
-        //if not, ask if you want to create ingredient (trigger event), plus show "similar results"
-        ingredientNotFound(item);
+        //if not, plus show "similar results"
+        ingredientNotFound(item, amount);
     }
-    private void ingredientNotFound(Ingredient item){
+    Inventory(){
+        //load all existing foods from selected ones
+    }
+    private void ingredientNotFound(Ingredient item, Double amount){
         //search existing ingredients for similarly spelled items, or items that contain the input (if we have time lmao)
-
+        //ask if you want to create ingredient (trigger event)
+        String output = "Do you want to create the new ingredient '" +item+"'?";
+        if(EventHandler.yesOrNo()){
+            //createNewIngredient(item);
+        }
+        //create new ingredient in "database"
+        //item.newItem();
     }
 }
