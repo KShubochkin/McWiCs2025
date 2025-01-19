93b178d35e00c30c4932e35be295c64ce99bcc93
liae15 <liaerisson@gmail.com>
2025-01-19 11:54:35 -0500

    Update dataset.py

--- src/main/java/dataset.py
+++ src/main/java/dataset.py
@@ -3,12 +3,10 @@ pip install kaggle
 import kagglehub
 
 # Download latest version
-path = kagglehub.dataset_download("paultimothymooney/recipenlg")
+path = kagglehub.dataset_download("thedevastator/better-recipes-for-a-better-life")
 
 print("Path to dataset files:", path)
 
-
-#Note to group: these are the links for setup, I have a token for the API but genuinely don't understand github/python enough for this
 #https://github.com/Kaggle/kagglehub/blob/main/README.md#download-dataset
 #https://www.kaggle.com/docs/api#authentication
 #https://www.kaggle.com/datasets/paultimothymooney/recipenlg/data
